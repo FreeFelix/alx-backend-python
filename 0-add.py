0d08f19546fd5c122bd5e290cacda3ec74b3145b
root <root@DESKTOP-7RGN2MU>
2024-05-03 00:38:24 +0200

    Add new commit

---
+++ 0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+    Type-annotation function
+'''
+
+def add(a: float, b: float) -> float:
+    '''
+        Function that takes two floats and returns their sum as a float
+    '''
+    return (a + b)
---
+++ 3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+type-annotated function
+'''
+
+
+def to_str(n: float) -> str:
+    '''converts float to strings
+    '''
+    return (str(n))
+
